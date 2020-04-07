89957a4eda74ddcfb5b64af3e133c9c2c0d8d8f2
deyad <deyadecember@gmail.com>
2020-04-07 08:57:25 +0300

    vork version

--- app/src/main/java/com/example/myapplicationl4/VideoActivity.java
+++ app/src/main/java/com/example/myapplicationl4/VideoActivity.java
@@ -1,6 +1,8 @@
 package com.example.myapplicationl4;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,10 +10,24 @@ import androidx.appcompat.app.AppCompatActivity;
 public class VideoActivity extends AppCompatActivity {
 
     VideoView videoView;
+    MediaController mediaController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.);
+        setContentView(R.layout.video_main);
+        videoView = (VideoView) findViewById(R.id.video_view);
+        mediaController = new MediaController(this);
+        playVideo();
     }
+
+    public void playVideo() {
+        String path = "android.resource://com.example.myapplicationl4/" + R.raw.video;
+        Uri uri = Uri.parse(path);
+        videoView.setVideoURI(uri);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
+        videoView.start();
+    }
+
 }
